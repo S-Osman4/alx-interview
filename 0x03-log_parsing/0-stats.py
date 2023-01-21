99b40bef9337a08d610cfab32bd59179e5f44dd4
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2023-01-22 02:38:31 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,29 @@
 #!/usr/bin/python3
-
-"""Script that reads stdin line by line and computes metrics"""
-
+'''Module for log parsing script.'''
 import sys
 
+if __name__ == "__main__":
+    total_size = 0
+    count = 1
+    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-def printsts(dic, size):
-    """ WWPrints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
+    def print_statistics():
+        print("File size: {}".format(total_size))
+        for key, value in sorted(codes.items()):
+            if (value):
+                print("{}: {}".format(key, value))
 
-
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
-
-count = 0
-size = 0
-
-try:
-    for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
-
-        stlist = line.split()
-        count += 1
-
-        try:
-            size += int(stlist[-1])
-        except:
-            pass
-
-        try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except:
-            pass
-    printsts(sts, size)
-
-
-except KeyboardInterrupt:
-    printsts(sts, size)
-    raise
+    try:
+        for line in sys.stdin:
+            tokens = line.split(" ")
+            total_size += int(tokens[-1])
+            code = int(tokens[-2])
+            if code in codes:
+                codes[code] += 1
+            if count % 10 == 0:
+                print_statistics()
+            count += 1
+    except Exception:
+        pass
+    finally:
+        print_statistics()

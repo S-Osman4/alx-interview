686e9652dbafbd7d362510be84cfbbd7dc5502f8
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2023-03-01 13:53:11 +0300

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
 """
-Island perimeter:
-    Returns the perimeter of the island described in grid.
+Island Perimeter:
+    returns the perimeter of the island described in grid
 """
 
 
 def island_perimeter(grid):
-
-    if not grid or not grid[0]:
-        return 0
-
-    rows, cols = len(grid), len(grid[0])
+    """island perimenter function"""
     perimeter = 0
-
-    for i in range(rows):
-        for j in range(cols):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                # check left
-                if j == 0 or grid[i][j-1] == 0:
-                    perimeter += 1
-                # check right
-                if j == cols-1 or grid[i][j+1] == 0:
-                    perimeter += 1
-                # check top
-                if i == 0 or grid[i-1][j] == 0:
-                    perimeter += 1
-                # check bottom
-                if i == rows-1 or grid[i+1][j] == 0:
-                    perimeter += 1
-
-    return perimeter
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+    return 
